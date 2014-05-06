a2ae8ba4279097fe9c1b0bcc63ed9cfa6ac2632c
storm <zhangqi.dev@qq.com>
2014-05-06 19:29:25 +0800

    Add integers.xml

--- app/src/main/java/me/storm/ninegag/ui/fragment/FeedsFragment.java
+++ app/src/main/java/me/storm/ninegag/ui/fragment/FeedsFragment.java
@@ -7,12 +7,10 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.Response;
