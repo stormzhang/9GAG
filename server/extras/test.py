93dffcb2ecc94554d5c65ac67da6a38a90b7e8ee
Yoni Mood <yonispark@gmail.com>
2016-08-19 23:59:27 +0300

    test html code with css

---
+++ server/extras/test.py
@@ -0,0 +1,4 @@
+
+with open("example.html", 'w') as f:
+	f.write('<p>Live Gist example in html</p>')
+	f.write('<script src="https://gist.github.com/MoodyUser/8b283b482cc517e711888dc287f7f2a6.js"></script>')
