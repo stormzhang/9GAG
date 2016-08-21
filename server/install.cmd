f99a235978d532f7f509427b251ad4c651676484
Yoni Mood <yonispark@gmail.com>
2016-08-22 00:04:19 +0300

    populating

--- server/install.cmd
+++ server/install.cmd
@@ -1,6 +1,7 @@
 
 @pip3 install -r requirements.txt%*
+@python manage.py makemigrations%*
 @python manage.py migrate%*
-@python manage.py install%*
-@start  http://localhost:8000/admin/login/ %*
+@python manage.py install.cmd%*
+@start  http://localhost:8000/admin/login/ *
 @python manage.py runserver%*
--- server/management/management/commands/populate.py
+++ server/management/management/commands/populate.py
@@ -3,13 +3,17 @@ from django.core.management import BaseCommand
 from django.contrib.sites.models import Site
 # from emessages import console as emc
 # from tips import console as tc
-
+from requests import get as _get
+from pprint import pprint
+from gistsapi.models import Gist
 
 class Command(BaseCommand):
     @staticmethod
     def populate():
-        pass
-
+        request = _get(r"https://api.github.com/gists")
+        gists = request.json()
+        for item in gists:
+            Gist.save_github_api(item)
 
 
     def handle(self, *args, **options):
