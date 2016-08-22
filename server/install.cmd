cbaa4a19e6eaac03871a41a320c28b216f6d6dad
Yoni Mood <yonispark@gmail.com>
2016-08-22 21:14:48 +0300

    install fix

--- server/install.cmd
+++ server/install.cmd
@@ -2,6 +2,6 @@
 @pip3 install -r requirements.txt%*
 @python manage.py makemigrations%*
 @python manage.py migrate%*
-@python manage.py install.cmd%*
+@python manage.py install%*
 @start  http://localhost:8000/admin/login/ *
 @python manage.py runserver%*
