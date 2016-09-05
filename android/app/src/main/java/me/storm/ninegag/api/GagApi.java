f1952b933e651e5be6bd1f92e66e714ae3295ccd
Yoni Mood <yonispark@gmail.com>
2016-09-05 18:41:13 +0300

    right connection

--- android/app/src/main/java/me/storm/ninegag/api/GagApi.java
+++ android/app/src/main/java/me/storm/ninegag/api/GagApi.java
@@ -4,7 +4,7 @@ package me.storm.ninegag.api;
  * Created by storm on 14-3-25.
  */
 public class GagApi {
-    private static final String HOST = "http://104.196.120.103/api/gists/";
+    private static final String HOST = "http://104.196.147.66/api/gists/";
 
     public static final String LIST = HOST + "";
 }
--- android/app/src/main/java/me/storm/ninegag/ui/adapter/FeedsAdapter.java
+++ android/app/src/main/java/me/storm/ninegag/ui/adapter/FeedsAdapter.java
@@ -73,54 +73,11 @@ public class FeedsAdapter extends CursorAdapter {
 
         holder.caption.setText(feed.title);
         //TODO: here you set the html for all scroll views
-        holder.gist.loadData("" +
-                "<body><p>Live Gist example in html</p><script src=\"https://gist.github.com/d95d5429655c1123ddca8730d5e8dc9f.js\"></script><link rel=\"stylesheet\" href=\"https://assets-cdn.github.com/assets/gist-embed-b03925b9c18fa42b22f168e7d17a7ca70ca50a4126cbfca9f91a1a8bc79b5905.css\"><div id=\"gist39255888\" class=\"gist\">" +
-                "    <div class=\"gist-file\">" +
-                "      <div class=\"gist-data\">" +
-                "        <div class=\"js-gist-file-update-container js-task-list-container file-box\">" +
-                "  <div id=\"file-wurst-client-v2-21-2-error-report-md\" class=\"file\">" +
-                "    " +
-                "  <div id=\"readme\" class=\"readme blob instapaper_body\">" +
-                "    <article class=\"markdown-body entry-content\" itemprop=\"text\"><blockquote>" +
-                "<p>This is an auto-generated error report for the <a href=\"https://www.wurst-client.tk\">Wurst Client</a>.</p>" +
-                "</blockquote>" +
-                "" +
-                "<h1><a id=\"user-content-description\" class=\"anchor\" href=\"#description\" aria-hidden=\"true\"><svg aria-hidden=\"true\" class=\"octicon octicon-link\" height=\"16\" version=\"1.1\" viewBox=\"0 0 16 16\" width=\"16\"><path d=\"M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z\"></path></svg></a>Description</h1>" +
-                "" +
-                "<p>An error occurred while processing events." +
-                "Event type: UpdateEvent</p>" +
-                "" +
-                "<h1><a id=\"user-content-stacktrace\" class=\"anchor\" href=\"#stacktrace\" aria-hidden=\"true\"><svg aria-hidden=\"true\" class=\"octicon octicon-link\" height=\"16\" version=\"1.1\" viewBox=\"0 0 16 16\" width=\"16\"><path d=\"M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z\"></path></svg></a>Stacktrace</h1>" +
-                "" +
-                "<pre><code>java.lang.NullPointerException" +
-                "" +
-                "</code></pre>" +
-                "" +
-                "<h1><a id=\"user-content-system-details\" class=\"anchor\" href=\"#system-details\" aria-hidden=\"true\"><svg aria-hidden=\"true\" class=\"octicon octicon-link\" height=\"16\" version=\"1.1\" viewBox=\"0 0 16 16\" width=\"16\"><path d=\"M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z\"></path></svg></a>System details</h1>" +
-                "" +
-                "<ul>" +
-                "<li>OS: Windows 7 (amd64)</li>" +
-                "<li>Java version: 1.8.0_101 (Oracle Corporation)</li>" +
-                "<li>Wurst version: 2.21.2 (latest: 2.21.2)</li>" +
-                "<li>Timestamp: 2016.08.27-10:35:50</li>" +
-                "</ul>" +
-                "</article>" +
-                "  </div>" +
-                "" +
-                "  </div>" +
-                "  " +
-                "</div>" +
-                "" +
-                "      </div>" +
-                "      <div class=\"gist-meta\">" +
-                "        <a href=\"https://gist.github.com/anonymous/d95d5429655c1123ddca8730d5e8dc9f/raw/58883dfd56c88ffa63d58b34a8c2e96d06035226/Wurst-Client-v2.21.2-Error-Report.md\" style=\"float:right\">view raw</a>" +
-                "        <a href=\"https://gist.github.com/anonymous/d95d5429655c1123ddca8730d5e8dc9f#file-wurst-client-v2-21-2-error-report-md\">Wurst-Client-v2.21.2-Error-Report.md</a>" +
-                "        hosted with ❤ by <a href=\"https://github.com\">GitHub</a>" +
-                "      </div>" +
-                "    </div>" +
-                "</div>" +
-                "</body>" +
-                "","","utf-8");
+        holder.gist.clearCache(true);
+        holder.gist.clearHistory();
+        holder.gist.getSettings().setJavaScriptEnabled(true);
+        // holder.gist.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        holder.gist.loadData(feed.script_url, "", "utf-8");
     }
 
     private Holder getHolder(final View view) {
